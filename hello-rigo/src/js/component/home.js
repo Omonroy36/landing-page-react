05fb078eab5dde0d64ca5425e51892281472c902
eliudjgomezg <eliudjgomezg@gmail.com>
2019-08-03 00:03:57 +0000

    jumbotron  and navbar modifications

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -2,14 +2,16 @@ import React, { Fragment } from "react";
 
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
+import { Cards } from "./cards";
+import { Footer } from "./footer";
 
 export function Home(props) {
 	return (
 		<Fragment>
 			<Navbar />
 			<Jumbotron />
-            <Cards />
-            <Footer />
+			<Cards />
+			<Footer />
 		</Fragment>
 	);
 }
