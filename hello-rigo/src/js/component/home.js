484db1079df698d4b7be3adb0d61e44d436d3c34
Omonroy36 <monroyorlando.36@gmail.com>
2019-08-03 00:04:17 +0000

    update card and footer

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -2,7 +2,8 @@ import React, { Fragment } from "react";
 
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
-
+import { Cards } from "./cards";
+import { Footer } from "./footer";
 export function Home(props) {
 	return (
 		<Fragment>
